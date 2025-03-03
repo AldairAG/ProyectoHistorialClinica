47fca57066c5111c057fe1f7be735fa518545dce
AldairAG <alagga02@gmail.com>
2025-03-03 02:07:20 -0600

    feat: update DoctorCardHome component to display user information and improve styling

--- frontend/src/components/cards/DoctorCardHome.jsx
+++ frontend/src/components/cards/DoctorCardHome.jsx
@@ -1,15 +1,39 @@
-const DoctorCardHome=()=>{
+import { useUser } from "../../hooks/useUser"
+
+const DoctorCardHome = () => {
+    const { user } = useUser()
     return (
-        <div className="border rounded-md h-64">
-            <div>
-                <label >Buenos dias.</label>
-                <label >Dr. Juan Perez</label>
-                <label >Tus citas hoy</label>
+        <div className="border rounded-md h-64 bg-blue-700 px-8 py-7 text-white shadow-md">
+            <div className="flex flex-col gap-2 mb-7">
+                <label className="text-sm font-semibold">Buenos dias.</label>
+                <label className="text-3xl">Dr. {user.nombre} {user.apellidoPaterno}</label>
+                <label className="font-semibold">Estado de la clinica hoy</label>
             </div>
-            <div>
-                <label >Consultas medica:{9}</label>
-                <label >Pacientes totales:{500}</label>
-                <label >otro campo:{9}</label>
+            <div className="flex gap-6">
+                <div className="flex gap-3">
+                    <div className=" border border-dotted border-sky-400 bg-sky-400 w-14 h-14 rounded-md flex items-center justify-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-clipboard2-pulse-fill" viewBox="0 0 16 16">
+                            <path d="M10 .5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5.5.5 0 0 1-.5.5.5.5 0 0 0-.5.5V2a.5.5 0 0 0 .5.5h5A.5.5 0 0 0 11 2v-.5a.5.5 0 0 0-.5-.5.5.5 0 0 1-.5-.5" />
+                            <path d="M4.085 1H3.5A1.5 1.5 0 0 0 2 2.5v12A1.5 1.5 0 0 0 3.5 16h9a1.5 1.5 0 0 0 1.5-1.5v-12A1.5 1.5 0 0 0 12.5 1h-.585q.084.236.085.5V2a1.5 1.5 0 0 1-1.5 1.5h-5A1.5 1.5 0 0 1 4 2v-.5q.001-.264.085-.5M9.98 5.356 11.372 10h.128a.5.5 0 0 1 0 1H11a.5.5 0 0 1-.479-.356l-.94-3.135-1.092 5.096a.5.5 0 0 1-.968.039L6.383 8.85l-.936 1.873A.5.5 0 0 1 5 11h-.5a.5.5 0 0 1 0-1h.191l1.362-2.724a.5.5 0 0 1 .926.08l.94 3.135 1.092-5.096a.5.5 0 0 1 .968-.039Z" />
+                        </svg>
+                    </div>
+                    <div className="flex flex-col">
+                        <label className="text-3xl">9</label>
+                        <label className="text-sm">Citas</label>
+                    </div>
+                </div>
+
+                <div className="flex gap-3">
+                    <div className="bg-green-400 w-14 h-14 rounded-md flex items-center justify-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-people-fill" viewBox="0 0 16 16">
+                            <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5.784 6A2.24 2.24 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.3 6.3 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5" />
+                        </svg>
+                    </div>
+                    <div className="flex flex-col">
+                        <label className="text-3xl">1578</label>
+                        <label className="text-sm">Pacientes totales</label>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- frontend/src/pages/user/Home.jsx
+++ frontend/src/pages/user/Home.jsx
@@ -6,19 +6,102 @@ const Home = () => {
         <div className="flex flex-col gap-4">
             <DoctorCardHome />
 
-            <div className="border rounded-md h-15 flex justify-around items-center">
-                <button className="bg-blue-200 h-10 rounded-md px-4">agregar nuevo paciente</button>
-                <button className="bg-blue-200 h-10 rounded-md px-4">agregar nueva consulta</button>
+            <div className="flex justify-around items-center gap-10">
+                <button className="w-full bg-blue-500 h-10 rounded-md px-4 text-white shadow-lg flex items-center justify-center gap-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-person-fill-add" viewBox="0 0 16 16">
+                        <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0m-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                        <path d="M2 13c0 1 1 1 1 1h5.256A4.5 4.5 0 0 1 8 12.5a4.5 4.5 0 0 1 1.544-3.393Q8.844 9.002 8 9c-5 0-6 3-6 4" />
+                    </svg>
+                    <label>Registrar nuevo paciente</label>
+                </button>
+                <button className="w-full bg-blue-500 h-10 rounded-md px-4 text-white flex items-center justify-center gap-2 shadow-lg">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-file-earmark-medical-fill" viewBox="0 0 16 16">
+                        <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0M9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1m-3 2v.634l.549-.317a.5.5 0 1 1 .5.866L7 7l.549.317a.5.5 0 1 1-.5.866L6.5 7.866V8.5a.5.5 0 0 1-1 0v-.634l-.549.317a.5.5 0 1 1-.5-.866L5 7l-.549-.317a.5.5 0 0 1 .5-.866l.549.317V5.5a.5.5 0 1 1 1 0m-2 4.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1 0-1m0 2h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1 0-1" />
+                    </svg>
+                    <label>
+                        Registrar nueva consulta
+                    </label>
+                </button>
+                <button className="w-full bg-blue-500 h-10 rounded-md px-4 text-white flex items-center justify-center gap-2 shadow-lg">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-calendar" viewBox="0 0 16 16">
+                        <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z" />
+                    </svg>
+                    <label>
+                        Agendar cita
+                    </label>
+                </button>
             </div>
 
-            <div className="border rounded-md h-36">
-                <label>En este espacio pueden ir distintas cards de informacion relavante
-                    algunas pueden ser nuevos pacientes, una grafica de las consultas por genero u hombres,mujeres y ni;os
-                    grafica de tipos de enfermedad que se han presentado este mes,etc
-                </label>
+            <div className="flex justify-between gap-4">
+                <div className="w-full flex h-25 bg-white shadow-md border border-gray-300 rounded-md p-4">
+                    <div className="border border-green-300 bg-green-100 rounded-full w-[50px] h-[50px] flex items-center justify-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="text-green-600 bi bi-people" viewBox="0 0 16 16">
+                            <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1zm-7.978-1L7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002-.014.002zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0M6.936 9.28a6 6 0 0 0-1.23-.247A7 7 0 0 0 5 9c-4 0-5 3-5 4q0 1 1 1h4.216A2.24 2.24 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816M4.92 10A5.5 5.5 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0m3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4" />
+                        </svg>
+                    </div>
+                    <div className="flex flex-col ms-4">
+                        <label className="text-xl font-semibold">100</label>
+                        <label className="text-sm">Pacientes consultados</label>
+                    </div>
+
+                </div>
+                <div className="w-full flex h-25 bg-white shadow-md border border-gray-300 rounded-md p-4">
+                    <div className="border border-green-300 bg-green-100 rounded-full w-[50px] h-[50px] flex items-center justify-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="text-green-600 bi bi-person-up" viewBox="0 0 16 16">
+                            <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m.354-5.854 1.5 1.5a.5.5 0 0 1-.708.708L13 11.707V14.5a.5.5 0 0 1-1 0v-2.793l-.646.647a.5.5 0 0 1-.708-.708l1.5-1.5a.5.5 0 0 1 .708 0M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0M8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4" />
+                            <path d="M8.256 14a4.5 4.5 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10q.39 0 .74.025c.226-.341.496-.65.804-.918Q8.844 9.002 8 9c-5 0-6 3-6 4s1 1 1 1z" />
+                        </svg>
+                    </div>
+                    <div className="flex flex-col ms-4">
+                        <label className="text-xl font-semibold">100</label>
+                        <label className="text-sm">Nuevos pacientes</label>
+                    </div>
+
+                </div>
+                <div className="w-full flex h-25 bg-white shadow-md border border-gray-300 rounded-md p-4">
+                    <div className="border border-sky-400 bg-sky-100 rounded-full w-[50px] h-[50px] flex items-center justify-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="text-sky-600 bi-file-medical-fill" viewBox="0 0 16 16">
+                            <path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2M8.5 4.5v.634l.549-.317a.5.5 0 1 1 .5.866L9 6l.549.317a.5.5 0 1 1-.5.866L8.5 6.866V7.5a.5.5 0 0 1-1 0v-.634l-.549.317a.5.5 0 1 1-.5-.866L7 6l-.549-.317a.5.5 0 0 1 .5-.866l.549.317V4.5a.5.5 0 1 1 1 0M5.5 9h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1 0-1m0 2h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1 0-1" />
+                        </svg>
+                    </div>
+                    <div className="flex flex-col ms-4">
+                        <label className="text-xl font-semibold">100</label>
+                        <label className="text-sm">Recetas emitidas</label>
+                    </div>
+
+                </div>
+                <div className="w-full flex h-25 bg-white shadow-md border border-gray-300 rounded-md p-4">
+                    <div className="border border-sky-400 bg-sky-100 rounded-full w-[50px] h-[50px] flex items-center justify-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="text-sky-600 bi-journal-medical" viewBox="0 0 16 16">
+                            <path d="M8 4a.5.5 0 0 1 .5.5v.634l.549-.317a.5.5 0 1 1 .5.866L9 6l.549.317a.5.5 0 1 1-.5.866L8.5 6.866V7.5a.5.5 0 0 1-1 0v-.634l-.549.317a.5.5 0 1 1-.5-.866L7 6l-.549-.317a.5.5 0 0 1 .5-.866l.549.317V4.5A.5.5 0 0 1 8 4M5 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m0 2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5" />
+                            <path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2" />
+                            <path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1z" />
+                        </svg>
+                    </div>
+                    <div className="flex flex-col ms-4">
+                        <label className="text-xl font-semibold">100</label>
+                        <label className="text-sm">historiales actualizados</label>
+                    </div>
+
+                </div>
+                <div className="w-full flex h-25 bg-white shadow-md border border-gray-300 rounded-md p-4">
+                    <div className="border border-amber-400 bg-amber-100 rounded-full w-[50px] h-[50px] flex items-center justify-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="text-amber-600 bi bi-coin" viewBox="0 0 16 16">
+                            <path d="M5.5 9.511c.076.954.83 1.697 2.182 1.785V12h.6v-.709c1.4-.098 2.218-.846 2.218-1.932 0-.987-.626-1.496-1.745-1.76l-.473-.112V5.57c.6.068.982.396 1.074.85h1.052c-.076-.919-.864-1.638-2.126-1.716V4h-.6v.719c-1.195.117-2.01.836-2.01 1.853 0 .9.606 1.472 1.613 1.707l.397.098v2.034c-.615-.093-1.022-.43-1.114-.9zm2.177-2.166c-.59-.137-.91-.416-.91-.836 0-.47.345-.822.915-.925v1.76h-.005zm.692 1.193c.717.166 1.048.435 1.048.91 0 .542-.412.914-1.135.982V8.518z" />
+                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                            <path d="M8 13.5a5.5 5.5 0 1 1 0-11 5.5 5.5 0 0 1 0 11m0 .5A6 6 0 1 0 8 2a6 6 0 0 0 0 12" />
+                        </svg>
+                    </div>
+                    <div className="flex flex-col ms-4">
+                        <label className="text-xl font-semibold">$18000</label>
+                        <label className="text-sm">Ingresos totales</label>
+                    </div>
+
+                </div>
             </div>
+
         </div>
     )
 }
 
-export default Home
+export default Home 
