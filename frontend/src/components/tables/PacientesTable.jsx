04fb3b06394aff03f3c49fc6a30b3c11621d12c0
AldairAG <alagga02@gmail.com>
2025-04-28 18:22:22 -0600

    Add initial implementation of PacientesTable component

---
+++ frontend/src/components/tables/PacientesTable.jsx
@@ -0,0 +1,10 @@
+const PacientesTable = () => {
+  return (
+    <div>
+      <h1>Pacientes Table</h1>
+      {/* Add your table implementation here */}
+    </div>
+  );
+}
+
+export default PacientesTable;
